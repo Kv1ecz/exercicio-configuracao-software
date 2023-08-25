63f6a817c4e93baec08bee067e58b300ebaa0acc
@esales.luis.silva <luis.silva@esales.com.br>
2023-08-25 15:14:50 -0300

    Adicionando métodos (a) e (d) do enunciado

--- Main.java
+++ Main.java
@@ -1,6 +1,37 @@
+import java.util.ArrayList;
 
 public class Main(){
     public static void main(String[] args) {
         
     }
+
+    public static int nOcorrencias(ArrayList<Integer> l, Integer el){
+        int qtdOcorrencias = 0;
+
+        l.forEach((i) -> {
+            if(i.equals(el)){
+                qtdOcorrencias++;
+            }
+        });
+
+        return qtdOcorrencias;
+    }
+
+    public static ArrayList<Integer> listRepeat(ArrayList< Integer > l){
+        ArrayList<Integer> listaAux = new ArrayList<Integer>();
+        ArrayList<Integer> listaRepetidos = new ArrayList<Integer>();
+
+
+        l.forEach((i) -> {
+            
+            if(listaAux.contains(i)){
+                listaRepetidos.add(i);
+            }
+
+            listaAux.add(i);
+        });
+
+        return listaRepetidos;
+    }
+
 }
